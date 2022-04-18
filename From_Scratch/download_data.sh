09543dc6c910e7b03bea8d2245af8cee6590d47c
Srushanth Baride <SrushanthBaride@gmail.com>
2022-04-18 19:49:40 +0100

    New !

--- From_Scratch/download_data.sh
+++
@@ -1,4 +0,0 @@
-kaggle competitions download -c machine-learning-in-science-2022
-mkdir data
-unzip machine-learning-in-science-2022.zip -d ./data
-rm machine-learning-in-science-2022.zip
---
+++ From_Scratch/init.bat
@@ -0,0 +1,4 @@
+kaggle competitions download -c machine-learning-in-science-2022
+mkdir data
+unzip machine-learning-in-science-2022.zip -d ./data
+rm machine-learning-in-science-2022.zip
