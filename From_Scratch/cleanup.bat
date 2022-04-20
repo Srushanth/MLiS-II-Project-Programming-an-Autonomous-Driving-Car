df5704d2d6390f1c415d07dc7468198492a4b5dc
Srushanth Baride <SrushanthBaride@gmail.com>
2022-04-20 21:36:50 +0100

    New model!

--- From_Scratch/cleanup.bat
+++ From_Scratch/cleanup.bat
@@ -1,2 +1,4 @@
 cd ./From_Scratch
+rm data.csv
 rmdir /S /Q data
+rmdir /S /Q models
--- From_Scratch/init.bat
+++ From_Scratch/init.bat
@@ -3,3 +3,7 @@ kaggle competitions download -c machine-learning-in-science-2022
 mkdir data
 unzip machine-learning-in-science-2022.zip -d ./data
 rm machine-learning-in-science-2022.zip
+mkdir models
+cd ./models
+mkdir speed
+mkdir angle
